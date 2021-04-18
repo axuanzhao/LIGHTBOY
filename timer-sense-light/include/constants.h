2125930f4a61c33e328b6ef23458e6035fd7faf6
axuanzhao <75474325+axuanzhao@users.noreply.github.com>
2021-04-18 13:04:57 +0100

    Add files via upload

---
+++ timer-sense-light/include/constants.h
@@ -0,0 +1,20 @@
+#ifndef __CONSTANTS_H
+#define __CONSTANTS_H
+
+#include <string>
+
+
+
+const std::string CFG_FILE_SECTION_GPIO = "Gpio";
+const std::string CFG_FILE_SECTION_WEB = "Web";
+
+const int DISABLE_STATUS = 0;
+const int ENABLE_STATUS = 1;
+const int INVALID_POSITION = -1;
+const int ONE_DAY_MINUTES = 1440;  // total minutes of a day
+
+const int MODE_ON = 0;
+const int MODE_OFF = 1;
+const int MODE_AUTO = 2;
+
+#endif
---
+++ timer-sense-light/src/timer_sense_light.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include <glog/logging.h>
+#include "config_loader.h"
+#include "util.h"
+#include "thread_web_handler.h"
+#include "thread_hardware_control.h"
+
+
+
+using namespace std;
+
+// global variables
+CConfigLoader gConfigLoader;
+
+void signalCatchSIGINT(int signalNumber);
+
+int main(int argc,char* argv[]) {
+  google::InitGoogleLogging(argv[0]);      // initialize Google's logging library
+  google::SetStderrLogging(google::INFO);  // set logging level
+
+  if (argc < 3) {
+    LOG(ERROR) << CUtil::getCurrentDate() << "\t" << "Usage example: ./timer_sense_light main_config_file time_range_config_file";
+    return 1;
+  }
+
+  if (!gConfigLoader.loadMainConfig(argv[1])) {
+    LOG(ERROR) << CUtil::getCurrentDate() << "\t" << "Failed to load main config file, please check";
+    return 1;
+  }
+
+  string timeRangeFile = argv[2];
+  if (!gConfigLoader.loadTimeRangeFromFile(timeRangeFile)) {  // load time range data from file
+    return 1;
+  }
+
+  /* signal handler */
+  CUtil::signalIgnore(SIGHUP);	// ignore SIGHUP signal, to ensure that current program can live after the terminal close 
+  CUtil::signalIgnore(SIGUSR1);	// ignore SIGUSR1 
+  CUtil::signalIgnore(SIGUSR2);	// ignore SIGUSR2 
+
+  /* register SIGINT signal handler */
+  struct sigaction saSIGINT;
+  memset(&saSIGINT, 0, sizeof(saSIGINT));
+  saSIGINT.sa_flags = 0;
+  saSIGINT.sa_handler = &signalCatchSIGINT;  // specify the signal handler 
+  sigaction(SIGINT, &saSIGINT, NULL);        // register 
+
+  /* create the web events handling thread */
+  pthread_t webHandlerThreadId = 0;
+  int createThreadRet = pthread_create(&webHandlerThreadId, NULL, threadWebHandler, NULL);
+  if (0 != createThreadRet) {
+    LOG(ERROR) << CUtil::getCurrentDate() << "\t" << "Failed to create the web events handling thread, quit";
+    return 1;
+  }
+
+  /* create the hardrware control thread */
+  pthread_t hardwareControlThreadId = 0;
+  createThreadRet = pthread_create(&hardwareControlThreadId, NULL, threadHardwareControl, NULL);
+  if (0 != createThreadRet) {
+    LOG(ERROR) << CUtil::getCurrentDate() << "\t" << "Failed to create the hardware control thread, quit";
+    return 1;
+  }
+
+  /* wait thread to finish */
+  pthread_join(webHandlerThreadId, NULL);
+  pthread_join(hardwareControlThreadId, NULL);
+
+  LOG(INFO) << CUtil::getCurrentDate() << "\t" << "Backend program exits";
+  return 0;
+}
+
+/**
+ * Signal SIGINT handler.
+ *
+ * @param signalNumber  Signal identifier.
+ */
+void signalCatchSIGINT(int signalNumber) {
+  LOG(INFO) << CUtil::getCurrentDate() << "\t" << "Received SIGINT, will exit...";
+  gConfigLoader.setThreadRunning(false);
+}
