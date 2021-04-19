cedafa0edd42b455299d07fb3ddd1831fe7f8462
axuanzhao <75474325+axuanzhao@users.noreply.github.com>
2021-04-19 14:11:58 +0100

    Delete SenseLight directory

--- SenseLight/script/common-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MAIN_CONFIG_FILE=$TIMER_SENSE_LIGHT_HOME/conf/main.conf
-TIME_RANGE_CONFIG_FILE=$TIMER_SENSE_LIGHT_HOME/conf/time-range.txt
-
-LOG_DIR=$TIMER_SENSE_LIGHT_HOME/logs
-mkdir -p $LOG_DIR
